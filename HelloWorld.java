5e1c154cc0998737892e32ba83caff39a1b63580
Shivpower <91133018+Shivpower@users.noreply.github.com>
2021-10-15 07:52:20 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -35,7 +35,7 @@
 public class HelloWorld {
   public static void main(String[] args) {
  System.out.println("Hello, everyone welcome jenkins");
-     System.out.println("Hello, everyone welcome jenkins");
+     System.out.println("jenkins");
      System.out.println("Hello, everyone welcome jenkins");
 }
 }
